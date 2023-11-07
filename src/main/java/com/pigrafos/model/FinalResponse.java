b2d4962d3c3640ac9d2c404204c3aef7839fb117
Luís Felipe <oluis10@gmail.com>
2023-11-07 18:21:19 -0300

    feat: adding api integration

---
+++ src/main/java/com/pigrafos/model/FinalResponse.java
@@ -0,0 +1,35 @@
+package com.pigrafos.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FinalResponse {
+
+    @JsonProperty("caminho_valido")
+    private boolean isValid;
+    @JsonProperty("quantidade_movimentos")
+    private int moves;
+
+    public FinalResponse() {
+    }
+
+    public FinalResponse(boolean isValid, int moves) {
+        this.isValid = isValid;
+        this.moves = moves;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public int getMoves() {
+        return moves;
+    }
+
+    @Override
+    public String toString() {
+        return "Final Response {" +
+                "isValid = " + isValid +
+                ", moves = " + moves +
+                " }";
+    }
+}
